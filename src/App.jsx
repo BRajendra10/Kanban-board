9a265b255062db82ad49ef1838180e0e54be7079
BRajendra10 <rajendrabehera8116@gmail.com>
2025-07-21 05:49:28 +0530

    layout is ready

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
 
   return (
-    <div className="header w-full h-screen">
+    <div className="header w-full h-screen bg-gray-200">
       <Navbar />
       <Main />
     </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,12 @@
-export default function Main(){
-    return(
-        <main className="main bg-gray-200"></main>
+export default function Main() {
+    return (
+        <main className="main">
+            <div className="main-container bg-gray-200">
+                <section className="bg-gray-100"></section>
+                <section className="bg-gray-100"></section>
+                <section className="bg-gray-100"></section>
+                <section className="bg-gray-100"></section>
+            </div>
+        </main>
     )
 }
