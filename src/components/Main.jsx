b64064c79f2b8486f6fc317510e96a6b75ce8914
BRajendra10 <rajendrabehera8116@gmail.com>
2025-07-21 06:10:08 +0530

    inner grid layout is done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,31 @@
+import { AiOutlineEllipsis } from "react-icons/ai";
+
 export default function Main() {
     return (
         <main className="main">
             <div className="main-container bg-gray-200">
-                <section className="bg-gray-100"></section>
-                <section className="bg-gray-100"></section>
-                <section className="bg-gray-100"></section>
-                <section className="bg-gray-100"></section>
+                <section>
+                    <Navs name={"To Do"} />
+                </section>
+                <section>
+                    <Navs name={"Progress"} />
+                </section>
+                <section>
+                    <Navs name={"Needs riview"} />
+                </section>
+                <section>
+                    <Navs name={"Done"} />
+                </section>
             </div>
         </main>
+    )
+}
+
+export function Navs({name}) {
+    return (
+        <div className="w-full h-[50px] flex justify-between items-center px-5">
+            <h2 className="text-xl">{name}</h2>
+            <button><AiOutlineEllipsis size={30} style={{ color: "gray" }} /></button>
+        </div>
     )
 }
