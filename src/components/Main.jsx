38e9621890a5dad0d18dfe7d7ce73dd9f206f700
BRajendra10 <rajendrabehera8116@gmail.com>
2025-07-21 08:30:07 +0530

    complete layout with one refrence task

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,9 +6,12 @@ export default function Main() {
             <div className="main-container bg-gray-200">
                 <section>
                     <Navs name={"To Do"} />
+                    <div className="articles">
+                        <Article />
+                    </div>
                 </section>
                 <section>
-                    <Navs name={"Progress"} />
+                    <Navs name={"In Progress"} />
                 </section>
                 <section>
                     <Navs name={"Needs riview"} />
@@ -21,11 +24,29 @@ export default function Main() {
     )
 }
 
-export function Navs({name}) {
+export function Navs({ name }) {
     return (
-        <div className="w-full h-[50px] flex justify-between items-center px-5">
-            <h2 className="text-xl">{name}</h2>
+        <div className="flex justify-between items-center px-5">
+            <h2 className="text-lg font-bold uppercase">{name}</h2>
             <button><AiOutlineEllipsis size={30} style={{ color: "gray" }} /></button>
         </div>
+    )
+}
+
+export function Article() {
+    return (
+        <article className="bg-gray-50 rounded-xl p-5">
+            <div className="tags flex gap-2">
+                <span className="bg-red-200 py-1 px-2 rounded-sm">Important</span>
+                <span className="bg-yellow-100 py-1 px-2 rounded-sm">Frontend Development</span>
+            </div>
+
+            <h2 className="heading text-lg font-semibold">Kanban project</h2>
+            <p className="discription">Create a kanban project. Make simple version with 4 section, todo, in progress, need rivew, done.</p>
+
+            <div className="info">
+                <span>🔗 2 Attachment</span>
+            </div>
+        </article>
     )
 }
