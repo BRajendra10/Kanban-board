e9a5ec3da708e0cd954ba1ca8179346521605799
BRajendra10 <rajendrabehera8116@gmail.com>
2025-07-24 20:49:50 +0530

    showing data in ui/dom

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,8 +1,31 @@
-import Article from "./Article";
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 import { AiOutlineEllipsis } from "react-icons/ai";
 import { FaPlus } from "react-icons/fa6";
+import Article from "./Article";
 
 export default function Activity() {
+    const [todata, setTodoData] = useState([]);
+    const [inProgress, setInProgress] = useState([]);
+    const [done, setDone] = useState([]);
+
+    async function getData() {
+        try {
+            const todoData = await axios.get(`http://localhost:3000/todo`);
+            const inProgressData = await axios.get(`http://localhost:3000/inprogress`);
+            const doneData = await axios.get(`http://localhost:3000/done`);
+            setTodoData(todoData.data);
+            setInProgress(inProgressData.data);
+            setDone(doneData.data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     return (
         <main className="main">
@@ -10,17 +33,23 @@ export default function Activity() {
                 <section>
                     <Navs name={"To Do"} />
                     <div className="articles">
-                        <Article />
+                        {todata.map((el) => <Article id={el.id} title={el.title} description={el.description} />)}
                     </div>
                 </section>
                 <section>
                     <Navs name={"In Progress"} />
+                    <div className="articles">
+                        {inProgress.map((el) => <Article id={el.id} title={el.title} description={el.description} />)}
+                    </div>
                 </section>
                 <section>
                     <Navs name={"Needs riview"} />
                 </section>
                 <section>
                     <Navs name={"Done"} />
+                    <div className="articles">
+                        {done.map((el) => <Article id={el.id} title={el.title} description={el.description} />)}
+                    </div>
                 </section>
             </div>
         </main>
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,15 +1,15 @@
 import { IoIosLink } from "react-icons/io";
 
-export default function Article() {
+export default function Article({id, title, description}) {
     return (
-        <article className="bg-gray-50 rounded-xl p-5">
+        <article className="bg-gray-50 rounded-xl p-5" key={id}>
             {/* <div className="tags flex gap-2">
                 <span className="bg-red-100 py-1 px-2 rounded-sm">Important</span>
                 <span className="bg-yellow-100 py-1 px-2 rounded-sm">Frontend Development</span>
             </div> */}
 
-            <h2 className="heading text-lg font-semibold">Kanban project</h2>
-            <p className="discription">Create a kanban project. Make simple version with 4 section, todo, in progress, need rivew, done.</p>
+            <h2 className="heading text-lg font-semibold">{title}</h2>
+            <p className="discription">{description}</p>
 
             <div className="info">
                 <span className="cursor-pointer flex items-center gap-2"> <IoIosLink /> 2 Attachment</span>
